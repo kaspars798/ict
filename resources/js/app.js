cbb2261738d33657bf6981e65cdf2830537f0d7d
elvabaltic/kasparsk <kasparsk@elvabaltic.lv>
2022-02-04 10:02:47 +0200

    first changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import HelloWorld from './views/app';
+
+const app = createApp({});
+
+app.component('hello-world', HelloWorld);
+
+app.mount('#app');
